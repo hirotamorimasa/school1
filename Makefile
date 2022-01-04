ee8823b999adb95822d5f989bfcabe887009eb07
Hiromasa Morita <s20c1119nu@s.chibakoudai.jp>
2022-01-04 14:30:34 +0900

    Update

--- Makefile
+++ Makefile
@@ -2,8 +2,7 @@
 #SPDX-License-Identifier:GPL-2.0
 #*Copyright (c) 2021 Hiromasa Morita. All rights reserved.
 
-
-objs = kadai2.o kadai4.o kadai5.o monte.o  
+objs = kadai2.o kadai4.o kadai5.o monte.o coprime.o  
 
 cfile: $(objs)
 	gcc -Wall -O2 -o cfile $(objs)
@@ -20,8 +19,12 @@ kadai5.o: kadai5.c
 monte.o: monte.c
 	gcc -c monte.c
 
+coprime.o: coprime.c
+	gcc -c coprime.c
+
 .PHOSY: clean
 
 clean:
 	rm -f cfile $(objs)
+
 
---
+++ coprime.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int coprime(int a, int b)
+{
+	int i;
+	int count = 0;
+	if(a > b)
+	{
+		for(i = 2; i < b; i++)
+		 {
+			 if(a % i == 0 && b % i == 0)
+			{
+				return 1;
+				count++;
+			}	
+		 	else if(count == 0)	return -1;
+		}
+	}
+	else 
+	{
+		for(i = 2; i < a; i++)
+		 {
+			 if(a%i == 0 && a%i==0)
+			{
+				return 1;
+				count++;
+			}
+		 	else if(count == 0)	return -1;
+		}
+	}
+}
+
+void coprime_print(void)
+{
+	int a, b;
+	srand(time(NULL));
+	
+	a = rand() % 100 + 1;
+	b = rand() % 100 + 1;
+	
+	printf("a=%d\tb=%d\n", a, b);
+	if(coprime(a,b) == -1)
+		printf("互いに素である.\n");
+	else if(coprime(a,b) == 1)
+		printf("互いに素ではない..\n");	
+
+}
+
+
+int main(void)
+{
+	coprime_print();
+	return 0;
+}
+
