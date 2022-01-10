379ade9ace0c4f1b3930fc95254d38f0daa4f7be
Hiromasa Morita <s20c1119nu@s.chibakoudai.jp>
2022-01-10 20:34:56 +0900

    Update

--- bingo.c
+++ bingo.c
@@ -5,18 +5,27 @@
 #include <time.h>
 
 #define NUMBER 25
+#define BINGO 75
 
+
+//カードの乱数
 int Ransu(void)
 {
-	return rand() % 75 +1;
+	return rand() % BINGO +1;
 }
 
-void Bingo_process(void)
+void start(int you)
+{
+	do
+	{
+		printf("1でスタート：");
+		scanf("%d", &you);
+	}while(you != 1);
+}
+
+void Bingo_card(int bingo[NUMBER])
 {
 	int i, j;
-	int bingo[NUMBER], cpu[NUMBER];
-	int you;
-	int count = 0;
 	srand(time(NULL));
 	
 	for(i = 0; i < NUMBER; i++)
@@ -39,61 +48,102 @@ void Bingo_process(void)
 			}while(same == 1);
 		}
 	}
-	
-	do
+}
+
+void Bingo_range(int bingo[NUMBER])
+{
+	int i;
+	for(i = 1; i <= NUMBER; i++)
 	{
-		j = 0;
-		for(i = 1; i <= NUMBER; i++)
+		printf("%2d\t", bingo[i-1]);
+		if(i % 5 == 0)
+			putchar('\n');
+	}
+}
+
+//同番を探す
+void research(int bingo[NUMBER],int lottery)
+{
+	int i;
+	for(i = 0; i < NUMBER; i++)
+	{
+		if(bingo[i] == lottery)
 		{
-			if(cpu[i] == bingo[i])	printf("__");
-			else	printf("%2d", bingo[i-1]);
-			putchar('\t');
-			if(i % 5 == 0)
+			printf("あった！\n");
+			break;
+		}
+	}
+
+}
+
+void Bingo_line(int bingo[NUMBER], int lottery[BINGO])
+{
+	int i, j;
+	for(i = 0; i < NUMBER; i++)
+	{
+		for(j = 0; j <= i; j++)
+		{
+			if(bingo[i] == lottery[j])
 			{
-				putchar('\n');
+				printf("__\t");
+				break;
+			}
+			else 
+			{
+				printf("%2d\t", bingo[i]);
+				break;
 			}
 		}
-		for(i = 0; i < NUMBER; i++)
+		if((i+1) % 5 == 0)
+			putchar('\n');
+	}
+}
+
+void Bingo_print(int bingo[NUMBER], int lottery[BINGO])
+{
+	int i, j;
+	int you;
+	for(i = 0; i < BINGO; i++)
+	{
+		start(you);
+		if(i == 0)	lottery[i] = Ransu();
+		else
 		{
-			do
-			{
-				printf("1でスタート：");
-				scanf("%d", &you);
-			}while(you != 1);
-			if(i == 0)	cpu[i] = Ransu();
-			else
-			{
-				int same;
+			int same;
 			do
 			{
 				same = -1;
-				cpu[i] = Ransu();
+				lottery[i] = Ransu();
 				for(j = 0; j < i; j++)
 				{
-					if(cpu[i] == cpu[j])
-					{
+					if(lottery[j] == lottery[i])
 						same = 1;
-					}
 				}
 			}while(same == 1);
-			}
-			printf("%d番目：%d\n", i + 1, cpu[i]);
-			if(cpu[i] == bingo[i])
-			{
-				printf("あった！\n");
-				count++;
-			}	
 		}
-	}while(count < NUMBER);
+		printf("%d番目：%d\n", i + 1, lottery[i]);
+		research(bingo, lottery[i]);
+		Bingo_line(bingo, lottery);
+		putchar('\n');
+	}
 }
 
-void Bingo_print(void)
+void Bingo_process(void)
 {
-	Bingo_process();
+	int i, j;
+	int bingo[NUMBER], lottery[BINGO];
+	int you;
+	int count = 0;
+	srand(time(NULL));
+	
+	Bingo_card(bingo);
+	Bingo_range(bingo);
+
+	Bingo_print(bingo, lottery);
 }
 
 int main(void)
 {
-	Bingo_print();
+	Bingo_process();
 	return 0;
 }
