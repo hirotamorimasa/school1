c44679b5c91208b4f488ff2182b3b8ad0443e248
Hiromasa Morita <s20c1119nu@s.chibakoudai.jp>
2022-01-12 21:00:57 +0900

    Update

--- bingo.c
+++ bingo.c
@@ -4,15 +4,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NUMBER 25
-#define BINGO 75
+#define CARD 25
+#define NUMBER 75
+#define BINGO 5
+#define REACH 4
+#define SLANT 2  //ナナメ
+#define AOU 12
 
 int count = 0;
 
 //カードの乱数
 int Ransu(void)
 {
-	return rand() % BINGO +1;
+	return rand() % NUMBER +1;
 }
 
 void start(int you)
@@ -24,12 +28,12 @@ void start(int you)
 	}while(you != 1);
 }
 
-void Bingo_card(int bingo[NUMBER])
+void Bingo_card(int bingo[CARD])
 {
 	int i, j;
 	srand(time(NULL));
 	
-	for(i = 0; i < NUMBER; i++)
+	for(i = 0; i < CARD; i++)
 	{
 		if(i == 0)	bingo[i] = Ransu();
 		else
@@ -51,10 +55,10 @@ void Bingo_card(int bingo[NUMBER])
 	}
 }
 
-void Bingo_range(int bingo[NUMBER])
+void Bingo_range(int bingo[CARD])
 {
 	int i;
-	for(i = 1; i <= NUMBER; i++)
+	for(i = 1; i <= CARD; i++)
 	{
 		printf("%2d\t", bingo[i-1]);
 		if(i % 5 == 0)
@@ -62,12 +66,10 @@ void Bingo_range(int bingo[NUMBER])
 	}
 }
 
-//同番を探す
-void research(int bingo[NUMBER],int lottery, int hit[NUMBER])
+void research(int bingo[CARD], int lottery, int hit[CARD])
 {
 	int i;
-	
-	for(i = 0; i < NUMBER; i++)
+	for(i = 0; i < CARD; i++)
 	{
 		if(bingo[i] == lottery)
 		{
@@ -78,11 +80,119 @@ void research(int bingo[NUMBER],int lottery, int hit[NUMBER])
 	}
 }
 
-void Bingo_line(int bingo[NUMBER], int hit[NUMBER])
+//初期化
+void Init(int aou[AOU])
+{
+	int i;
+	for(i = 0; i < AOU; i++)
+		aou[i] = 0;
+}
+
+void Yoko_reach(int bingo[CARD], int hit[NUMBER], int aou[AOU])
+{
+	int i, j, k;
+	
+	j = 0;
+	for(i = 1 ; i <= BINGO; i++)
+	{
+		for(j; j < BINGO * i; j++)
+		{
+			for(k = 0; k < count; k++)
+			{
+				if(bingo[j] == hit[k])
+					aou[i-1] += 1;
+			}
+		}
+	}
+}
+
+void Tate_reach(int bingo[CARD], int hit[NUMBER], int aou[AOU])
+{
+	int i, j, k;
+	
+	for(i = 0; i < BINGO; i++)
+	{
+		for(j = i; j < CARD - 4 + i; j += 5)
+		{
+			for(k = 0; k < count; k++)
+			{
+				if(hit[k] == bingo[j])
+					aou[i + 5] += 1;
+			}
+		}
+	}
+}
+
+void Right_Naname_reach(int bingo[CARD], int hit[NUMBER], int aou[AOU])
+{
+	int i, j;
+	
+	for(i = 0; i < CARD; i += 6)
+	{
+		for(j = 0; j < count; j++)
+		{
+			if(hit[j] == bingo[i])
+				aou[10] += 1;
+		}
+	}
+}
+	
+void Left_Naname_reach(int bingo[CARD], int hit[NUMBER], int aou[AOU])
+{
+	int i, j;
+	
+	for(i = 4; i < CARD - 4; i += 4)
+	{
+		for(j = 0; j < count; j++)
+		 {
+			if(hit[j] == bingo[i])
+				aou[11] += 1;
+		}
+	}
+}
+
+void Reach(int bingo[CARD], int hit[NUMBER], int aou[AOU])
+{
+	int i, j, k;
+	Init(aou);
+
+	Yoko_reach(bingo, hit, aou);
+	Tate_reach(bingo, hit, aou);
+	Right_Naname_reach(bingo, hit, aou);
+	Left_Naname_reach(bingo, hit, aou);
+}
+
+void Reach_print(int bingo[CARD], int hit[NUMBER], int aou[NUMBER])
+{
+	int i;
+	int r_count = 0, b_count = 0;
+	
+	//AOU = 12, SLANT = 2
+	for(i = 0; i < AOU - SLANT; i++)
+	{
+		if(aou[i] == REACH)
+			r_count++;
+		else if(aou[i] == BINGO)
+			b_count++;
+	}
+
+	for(i = AOU - SLANT; i < AOU; i++)
+	{
+		if(aou[i] == REACH)
+			r_count++;
+		else if(aou[i] == BINGO)
+			b_count++;
+	}
+
+	printf("\n%2dリーチ\t%2dビンゴ\n", r_count, b_count);
+	putchar('\n');
+}
+
+void Bingo_line(int bingo[CARD], int hit[CARD])
 {
 	int i, j;
 	int zou;	
-	for(i = 0; i < NUMBER; i++)
+	for(i = 0; i < CARD; i++)
 	{
 		for(j = 0; j < count; j++)
 		{
@@ -100,11 +210,15 @@ void Bingo_line(int bingo[NUMBER], int hit[NUMBER])
 	}
 }
 
-void Bingo_print(int bingo[NUMBER], int lottery[BINGO], int hit[NUMBER])
+void Bingo_print(int bingo[CARD], int lottery[NUMBER], int hit[CARD])
+
 {
 	int i, j;
 	int you;
-	for(i = 0; i < BINGO; i++)
+	int aou[AOU];
+	
+	Init(aou);
+	for(i = 0; i < NUMBER; i++)
 	{
 		start(you);
 		if(i == 0)	lottery[i] = Ransu();
@@ -124,6 +238,8 @@ void Bingo_print(int bingo[NUMBER], int lottery[BINGO], int hit[NUMBER])
 		}
 		printf("%d番目：%d\n", i + 1, lottery[i]);
 		research(bingo, lottery[i], hit);
+		Reach(bingo, hit, aou);
+		Reach_print(bingo, hit, aou);
 		Bingo_line(bingo, hit);
 		putchar('\n');
 	}
@@ -132,14 +248,13 @@ void Bingo_print(int bingo[NUMBER], int lottery[BINGO], int hit[NUMBER])
 void Bingo_process(void)
 {
 	int i, j;
-	int bingo[NUMBER], lottery[BINGO], hit[NUMBER];
+	int bingo[CARD], lottery[NUMBER], hit[CARD];
 	int you;
 	int count = 0;
 	srand(time(NULL));
 	
 	Bingo_card(bingo);
 	Bingo_range(bingo);
-
 	Bingo_print(bingo, lottery, hit);
 }
 
---
+++ dentaku.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void Dentaku_process(void)
+{
+}
+
+void Dentaku_print(void)
+{
+	int you;
+	int sum; 
+	Dentaku_process();
+}
+
+int main(void)
+{
+	Dentaku_print();
+	return 0;
+}
